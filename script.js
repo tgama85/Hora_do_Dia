d2023e062f7071aa4dd2f66e9b1ecf58f130b83f
tgama85 <tgama85@gmail.com>
2021-05-02 17:17:45 -0300

    Alterações no HTML5 e JS

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ function carregar(){
   msg.innerHTML = `Agora são ${hora} hora(s).`
   if (hora >= 0 && hora < 12){
     //Bom dia
-    imagem.src = 'manha.png'
+    imagem.src = 'img/manha.png'
     document.body.style.background = 'url(https://images.pexels.com/photos/1090745/pexels-photo-1090745.jpeg?cs=srgb&dl=pexels-elias-tigiser-1090745.jpg&fm=jpg)'
   } else if (hora >=12 && hora <= 18){
     //Boa tarde
-    imagem.src = 'tarde.png'
+    imagem.src = 'img/tarde.png'
     document.body.style.background = 'url(https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)'
   } else{
     //Boa noite
-    imagem.src = 'noite.png'
+    imagem.src = 'img/noite.png'
     document.body.style.background = 'url(https://images.pexels.com/photos/1933239/pexels-photo-1933239.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)'
   }
 }
